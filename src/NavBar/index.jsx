c06b084d8bfcf85289d692fd3f8915ee6d086f47
William Ellison <waellison@gmail.com>
2022-04-15 12:16:43 -0400

    Added a navigation bar.

---
+++ src/NavBar/index.jsx
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <menu>
+                <li>
+                    <a href={"/"}>Home</a>
+                </li>
+                <li>
+                    <a href={"/posts/all"}>All Posts</a>
+                </li>
+            </menu>
+        </nav>
+    )
+}
+
+export default NavBar
